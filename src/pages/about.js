d8f0d10a274d118ce31650ce16efc15fcaa4f260
Alp Kaan Aksu <alpkaksu@gmail.com>
2021-02-27 11:12:29 +0300

    update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,38 @@ import React from 'react'
 const About = () => (
     <div>
         <h2>About</h2>
-        <p>I'm learning Mandarin Chinese and record words and characters I learn here. I hope to create a useful Chinese etimological dictionary at the end of my Chinese learning journey.</p>
+        <p>I'm learning Mandarin Chinese and record words and characters I learn here. I hope to create a useful Chinese etimological dictionary at the end of my Chinese learning journey.
+            <br/><br></br><em>There is never an end to learning another language, the above sentence pretty much means 'forever'. I don't even expect to have it <strong>complete</strong> in the sense of a dictionary. This is my hobbyist approach.</em>
+        </p>
+        <br/>
+        <p>Send an email to <a href="mailto:alpkaksu@gmail.com">alpkaksu@gmail.com</a> for feedback and corrections.</p>
+        <br/>
+        <h3>How I collect the data</h3>
+        <p>I am not an expert in Chinese etymology (I am just learning Chinese) so I get the information from varied sources.
+            <br/><br/>Two major sources for the data on this website are:
+            <ul>
+                <li><a href="https://en.wiktionary.org/wiki/%E6%BA%90">Wiktionary</a></li>
+                <li><a href="https://hanziyuan.net/#%E6%BA%90">Hanziyuan of Richards Sears (<em>aka</em> 'uncle Hanzi')</a></li>
+            </ul>
+            But I also try to verify the gathered information from other sources whenever possible. The information on this website may not always be 100% accurate and it is also not suitable for scientific use.
+            Having said that, I still think every single entry on this website serves as a great help for learning Hanzi. Think of them as mnemonics that are rooted in reality to some extent.
+        </p>
+        <br/>
+        <h3>Why collect and paraphrase the information that is already available to everyone?</h3>
+        <p>There are some personal and some practical reasons I can think of:
+            <ul>
+                <p><strong>Personal:</strong></p>
+                <li>I like etymology. It gets even more interesting when you are dealing with a writing system like Hanzi. It feels great to acquire a deeper understanding of the characters.</li>
+                <li>I like recording what I learn.</li>
+                <li>I like sharing interesting information with others. Creating this website is just an alternative of sending hundreds of screenshots of etymological explanations to my friends who don't even learn or care about Chinese.</li>
+                <li>I learn better this way.</li>
+                <br/>
+                <p><strong>Practical:</strong></p>
+                <li>It is nice to have a go-to resource.</li>
+                <li>I don't like how the relevant information is presented. They are not easy to understand. I try to make the information easy to understand by stating everything clearly. I think you'll understand when you make a few searches on this website.</li>
+                <li>Clean interface, no distractions.</li>
+            </ul>
+        </p>
     </div>
 )
 
